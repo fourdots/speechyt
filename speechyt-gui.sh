61fa6f4518279fb31182dcb1e226a67d7433df78
fourdots <rad@fourdots.co>
2025-11-07 22:40:43 +0100

    🔧 Fix GUI button to work like mouse/keyboard shortcuts
    - Removed redundant state checking in GUI button
    - toggle_recording.sh already handles start/stop internally
    - Now works consistently: click to start, click to stop
    - Same behavior as mouse button 4 and Right Ctrl

--- speechyt-gui.sh
+++ speechyt-gui.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 # SpeechyT GUI Button - Click to toggle recording
 
-LOCKFILE="$HOME/speechyt/recording.lock"
-
-if [ -f "$LOCKFILE" ]; then
-    # Currently recording - stop it
-    notify-send "🛑 SpeechyT" "Stopping recording..." -t 1000
-    bash "$HOME/speechyt/toggle_recording.sh"
-else
-    # Not recording - start it
-    notify-send "🎤 SpeechyT" "Starting recording..." -t 1000
-    bash "$HOME/speechyt/toggle_recording.sh"
-fi
+# Just call toggle_recording.sh - it handles the state internally
+bash "$HOME/speechyt/toggle_recording.sh"
